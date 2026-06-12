ce595b34ce33abb36a11f56fd6ae2343d84a4849
li leo <faboto@yahoo.com>
2026-06-12 11:21:35 +0200

    add ui

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, send_from_directory
 from app.api.routes.chat import chat_router
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 app.register_blueprint(chat_router, url_prefix="/api")
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return jsonify({"message": "Chatbot API is running!"})
+    # Envoie le fichier index.html du dossier static
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', debug=True, port=8080)
